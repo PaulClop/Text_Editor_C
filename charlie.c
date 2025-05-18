41da54fec9cb2b3ac210a58a2f1de9d9a3704665
Paul <cloppauleduard@gmail.com>
2025-05-18 16:10:01 +0300

    3

--- charlie.c
+++ charlie.c
@@ -1,50 +1,81 @@
+/* LIBRARI */ 
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/* DATA */
+
 struct termios orig_termios;
+
+/* TERMINAL */
+
+// afisare eroare si terminare program
+void die(const char *s) 
+{
+  perror(s);
+  exit(1);
+}
 
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
 }
 
 void enableRawMode() 
 {
     // salveaza si inregistreaza aplicarea la terminare programului setarile default terminal
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
 
-    // opreste CTRL - M (carriage) | CTRL - S/Q
-    raw.c_iflag &= ~(ICRNL | IXON);
+    // opreste CTRL - M (carriage) | CTRL - S/Q | restul de siguranta, legacy
+    raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
+
+    raw.c_oflag &= ~(OPOST); // opreste "\n" in "\r\n"
     
     // opreste afisare characterelor in terminal | canonical mode | CTRL - V | CTRL - C/Z
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //aplicare setari, flash la unread characters
+    // timp de asteptare pentru input, character control
+    raw.c_cc[VMIN] = 0; // minimul de bytes cititi pentru read() sa returneze
+    raw.c_cc[VTIME] = 1; // ./10 secunde
+
+    //aplicare setari, flash la unread characters
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw))
+        die("tcsetattr");
 }
+
+/* INITIALIZARE */
 
 int main()
 {
     enableRawMode();
 
     // Citeste fiecare bit / charater
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    while(1)
     {
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) // fara eroare cand time out
+            die("read");
+    
         if (iscntrl(c)) // non-printable/control character
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } 
 
         else 
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+
+        if(c == 'q')
+            break;
     }
 
     return 1;
