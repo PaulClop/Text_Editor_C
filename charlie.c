673ed198e43038be5ffbd7a90ec37f47ff047f04
Paul <cloppauleduard@gmail.com>
2025-05-20 19:02:27 +0300

    5

--- charlie.c
+++ charlie.c
@@ -1,8 +1,9 @@
 /* LIBRARI */ 
-#include <ctype.h>
+#include <ctype.h>  
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -12,31 +13,43 @@
 
 /* DATA */
 
-struct termios orig_termios;
+struct editorConfig
+{
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 
 /* TERMINAL */
 
 // afisare eroare si terminare program
 void die(const char *s) 
 {
-  perror(s);
-  exit(1);
+    // stergere ecran
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    perror(s);
+    exit(1);
 }
 
 void disableRawMode()
 {
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() 
 {
     // salveaza si inregistreaza aplicarea la terminare programului setarile default terminal
-    if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     // opreste CTRL - M (carriage) | CTRL - S/Q | restul de siguranta, legacy
     raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
@@ -70,11 +83,41 @@ char editorReadKey()
     return c;
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) // Terminal IOCtl (input/output control) Get WINdow SiZe
+    {
+        return -1;
+    }
+
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /* IESIRE */
+
+// pentru ~ la inceputul liniilor 
+void editorDrawRows()
+{
+    int y;
+    for (y = 0; y < E.screenrows; y++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
 
 void editorRefreshScreen()
 {
     write(STDOUT_FILENO, "\x1b[2J", 4); // <esc>[ - Escape (x1b = 0x1B = 27), 2 - tot ecranul, J - clear screen
+    write(STDOUT_FILENO, "\x1b[H", 3); // pozitioneaza mouse-ul la inceput
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /* INTRARE */
@@ -87,6 +130,8 @@ void editorProcessKeypress()
   switch (c)
   {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
   }
@@ -94,9 +139,16 @@ void editorProcessKeypress()
 
 /* INITIALIZARE */
 
+// preia dimensiunile ecranului
+void initEditor()
+{
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main()
 {
     enableRawMode();
+    initEditor();
 
     while (1)
     {
