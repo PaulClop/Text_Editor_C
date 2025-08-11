7c67c6f48ce33cf5644a87753420bb5dcdacd87e
Paul <185709243+PaulClop@users.noreply.github.com>
2025-08-11 18:02:28 +0300

    Citire o linie dintr-un fisier ca parametru

--- charlie.c
+++ charlie.c
@@ -1,4 +1,10 @@
 /* LIBRARI */
+
+// POSIX
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -219,16 +225,30 @@ int getWindowSize(int *rows, int *cols)
 }
 
 /*** fisier i/o ***/
-void editorOpen()
+void editorOpen(char *filename)
 {
-    char *line = "Hello, world!";
-    ssize_t linelen = 13;
+    FILE *fp = fopen(filename, "r");
+    if (!fp)
+        die("fopen");
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0; // capacitate linie bytes
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1)
+    {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')) // sterege newline si carriage
+            linelen--;
+
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+
+    free(line); // eliberare memorie
+    fclose(fp);
 }
 
 /* append buffer */
@@ -409,11 +429,14 @@ void initEditor()
         die("getWindowSize");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     enableRawMode();
     initEditor();
-    editorOpen();
+    if (argc >= 2)
+    {
+        editorOpen(argv[1]);
+    }
 
     while (1)
     {
